0da6f1abc8bb966b3521e13ae8dc93357ba15cdf
brubsby <brubsbybrubsby@gmail.com>
2020-02-17 00:12:03 -0600

    remove old requirements

--- desktop/main.js
+++ desktop/main.js
@@ -1,8 +1,6 @@
 // Modules to control application life and create native browser window
 const {app, BrowserWindow, Menu} = require('electron')
 const path = require('path')
-const SVG = require("@svgdotjs/svg.js")
-const fengari = require("@mstrodl/fengari")
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
